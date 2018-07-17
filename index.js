7f4deb48a68716b4afe81eedca9fbb882509b14f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 21:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ class BoardMember {
   releasePressStatement() {console.log(`You will see great things from Scuber.`)};
   sayHi() {console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)};
 }
+
+class Ceo extends BoardMember {
+  hireEmployee() {console.log(`Welcome aboard!`)};
+
+}
