67723edc43ec168b3de244f8e950c42a49f8b65c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 21:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ class BoardMember {
     this.homeState = homeState;
     this.training = training;
   }
+  veto() {console.log(`No, I must disagree`)};
+  veto() {console.log(`No, I must disagree`)};
+  veto() {console.log(`No, I must disagree`)};
+  veto() {console.log(`No, I must disagree`)};
 
-  sayHello() {
-    console.log(`Hello, my name is ${this.name}`);
-  }
+  sayHi() {console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)};
 }
