61133f1c3a016eedeb6eb71d2062d2883f376105
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 21:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ class BoardMember {
   approve() {console.log(`You can do that!`)};
   doCharity() {console.log(`I like to help people.`)};
   releasePressStatement() {console.log(`You will see great things from Scuber.`)};
-
   sayHi() {console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)};
 }
